3a678837cec9b3b443d0c7fb49c17a8551cd28ae
raberin <rmvaberin@gmail.com>
2020-03-27 15:39:37 -0700

    Part 2 - Done

--- flask_blog.py
+++ flask_blog.py
@@ -1,19 +1,33 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': 'March 27, 2020'
+    },
+    {
+        'author': 'Roenz Aberin',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date_posted': 'March 28, 2020'
+    }
+]
 
 
 @app.route('/')
 @app.route('/home')
 def home():
     # name = request.args.get("name", "World")
-    return f'<h1>Home Page</h1>'
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    # name = request.args.get("name", "World")
-    return f'<h1>About Page</h1>'
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
