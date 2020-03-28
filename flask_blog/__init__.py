1ab9987883a87d32c5ff75a2f3377721d264a65f
raberin <rmvaberin@gmail.com>
2020-03-28 16:02:25 -0700

    Part 6: User Authentication - Done

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,15 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1f7f95379f823e09d87a6da0b35116d7'
-
-# Set where SQLite DB is created => Root folder
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
-# Instantiate SQLite DB
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 # Must do it here
 from flask_blog import routes
